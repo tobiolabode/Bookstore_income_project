7d8339ba52abcbe0e7c26f9e21445eb8da496b35
tobiolabode <tobi@tobiolabode.com>
2020-04-11 14:03:51 +0100

    Found naivgation to starting div of bookstore info

--- scaper.py
+++ scaper.py
@@ -6,6 +6,11 @@ page = requests.get("https://www.waterstones.com/bookshops/viewall")
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
-print(soup.head.body)
+main_container_div_element = soup.body.div
+first_sibling_div = soup.body.div.div
+parent_first_sibling_div = first_sibling_div.parent
 
-# print(soup.prettify())
+
+find_div = main_container_div_element.find_all('div', class_="main-page row")
+print(find_div[0])
+# print(soup.body.div.div.next_sibling.prettify())
