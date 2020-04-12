2b464c4835b9b89ee1f93d8bf830224707f70b2b
tobiolabode <tobi@tobiolabode.com>
2020-04-12 14:43:58 +0100

    add loop to extract name and address from bookstore_info varible

--- scaper.py
+++ scaper.py
@@ -15,7 +15,15 @@ find_div = main_container_div_element.find_all('div', class_="main-page row")
 # print(find_div[0])
 
 find_div_2 = find_div[0].find_all('div', class_='shops-directory-list span12 alpha omega section')
-print(find_div_2)
+# print(find_div_2)
 
 Bookstore_info = find_div_2[0].find_all('div', class_='shop-item span6 mobile-span12')
-print(Bookstore_info)
+# print(Bookstore_info)
+
+
+for each_bookstore in Bookstore_info:
+    name_of_bookstore = each_bookstore.find('a', class_='title link-invert')
+    address_of_bookstore = each_bookstore.find('a', class_='shop-address')
+
+    print(name_of_bookstore.text.strip())
+    print(address_of_bookstore.text.strip())
