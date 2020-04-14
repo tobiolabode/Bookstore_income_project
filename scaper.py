3b2f392c35667be3ecf93f13c8d160ce7bbb91c9
tobiolabode <tobi@tobiolabode.com>
2020-04-14 17:04:16 +0100

    added mutiple pages feature

--- scaper.py
+++ scaper.py
@@ -1,37 +1,44 @@
 import csv
+import time
 from bs4 import BeautifulSoup
 import requests
 
+pages = [str(i) for i in range(1, 15)]
 
-page = requests.get("https://www.waterstones.com/bookshops/viewall")
+for each_page in pages:
+    page = requests.get("https://www.waterstones.com/bookshops/viewall" + '/page/' + each_page)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    if page.status_code != 200:
+        print('REQUEST DID NOT GO THROUGH: PAGE {} '.format(each_page))
 
-main_container_div_element = soup.body.div
-first_sibling_div = soup.body.div.div
-parent_first_sibling_div = first_sibling_div.parent
+    time.sleep(10)
 
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-find_div = main_container_div_element.find_all('div', class_="main-page row")
-# print(find_div[0])
+    main_container_div_element = soup.body.div
 
-find_div_2 = find_div[0].find_all('div', class_='shops-directory-list span12 alpha omega section')
-# print(find_div_2)
+    find_div = main_container_div_element.find_all('div', class_="main-page row")
+    # print(find_div[0])
 
-Bookstore_info = find_div_2[0].find_all('div', class_='shop-item span6 mobile-span12')
-# print(Bookstore_info)
+    find_div_2 = find_div[0].find_all(
+        'div', class_='shops-directory-list span12 alpha omega section')
+    # print(find_div_2)
 
-with open('dataset.csv', 'w') as dataset_file:
-    dataset_writer = csv.writer(dataset_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    print('Bookstore name, Street_name, Town, Postcode')
-    dataset_writer.writerow(['Bookstore name,  Street_name, Town, Postcode'])
-    for each_bookstore in Bookstore_info:
-        name_of_bookstore = each_bookstore.find('a', class_='title link-invert')
-        address_of_bookstore = each_bookstore.find('a', class_='shop-address')
-        seprated_address = ' '.join(address_of_bookstore.text.split())
+    Bookstore_info = find_div_2[0].find_all('div', class_='shop-item span6 mobile-span12')
+    # print(Bookstore_info)
 
-        print(name_of_bookstore.text.strip(), seprated_address)
+    with open('dataset.csv', 'w') as dataset_file:
+        dataset_writer = csv.writer(dataset_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        print('Page Number {}'.format(each_page))
+        print('Bookstore name, Street_name, Town, Postcode')
+        dataset_writer.writerow(['Bookstore name,  Street_name, Town, Postcode'])
+        for each_bookstore in Bookstore_info:
+            name_of_bookstore = each_bookstore.find('a', class_='title link-invert')
+            address_of_bookstore = each_bookstore.find('a', class_='shop-address')
+            seprated_address = ' '.join(address_of_bookstore.text.split())
 
-        print('\n')
+            print(name_of_bookstore.text.strip(), seprated_address)
 
-        dataset_writer.writerow([name_of_bookstore.text.strip(), seprated_address])
+            print('\n')
+
+            dataset_writer.writerow([name_of_bookstore.text.strip(), seprated_address])
