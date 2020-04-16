1fc96c586c2175d38446b61682084798f13be721
tobiolabode <tobi@tobiolabode.com>
2020-04-16 14:39:02 +0100

    Trying to troubleshoot issue with pandas parsing the dataset incorrectly

--- analysis.py
+++ analysis.py
@@ -1,1 +1,10 @@
 import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+
+Postcodes = ['EN', 'IG', 'RM', 'DA', 'BR', 'TN', 'CR', 'SM', 'KT',
+             'TW', 'HA', 'UB', 'E', 'EC', 'N', 'NW', 'SE', 'SW', 'W', 'WC']
+
+dataset_df = pd.read_csv('dataset_no_quotes.csv')
+print(dataset_df.head(10))
+
+# print(dataset_df.dtypes)
