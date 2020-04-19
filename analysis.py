cd3ce63c6d1108eb110eb58e82d105ce652a217b
tobiolabode <tobi@tobiolabode.com>
2020-04-19 12:14:05 +0100

    Dropped town column. Also working out how to make a new regex pattern for london postcodes.

--- analysis.py
+++ analysis.py
@@ -10,6 +10,9 @@ Postcode_regex = '([A-Z]{1,2}[0-9][0-9A-Z]? [0-9][A-Z]{2})'
 
 dataset_df = pd.read_csv('dataset.csv')
 dataset_df['Postcode'] = dataset_df[' Street_name'].str.extract(Postcode_regex)
-print(dataset_df.head())
+dataset_df = dataset_df.drop('Town', axis=1)
+filtered_dataset_df = dataset_df[dataset_df.isin(Postcodes)]
+print(filtered_dataset_df.head(10))
+# print(dataset_df.head(10))
 
 # print(dataset_df.dtypes)
