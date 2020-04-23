5fa1850968a296b497092963199d2f855fcb4ae0
tobiolabode <tobi@tobiolabode.com>
2020-04-23 12:11:54 +0100

    Changing filtering system of dataset

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
 
 Postcodes = ['EN', 'IG', 'RM', 'DA', 'BR', 'TN', 'CR', 'SM', 'KT',
              'TW', 'HA', 'UB', 'E', 'EC', 'N', 'NW', 'SE', 'SW', 'W', 'WC']
@@ -9,14 +10,20 @@ Postcode_regex = '([A-Z]{1,2}[0-9][0-9A-Z]? [0-9][A-Z]{2})'
 # https://regexr.com/52ni1
 
 London_postcode_regex = '([^A-Z]E[0-9]|EC[0-9]|[^A-Z]N[0-9]|NW[0-9]|SE[0-9]|SW[0-9]|[^A-Z]W[0-9]|WC[0-9]|[E]N[0-9]|[I]G[0-9]|[R]M[0-9]|[D]A[0-9]|[B]R[0-9]|[T]N[0-9]|[C]R[0-9]|[S]M[0-9]|[K]T[0-9]|[T]W[0-9]|[H]A[0-9]|[U]B[0-9])'
+Greater_london_postcode_regex = '([E]N[0-9]|[I]G[0-9]|[R]M[0-9]|[D]A[0-9]|[B]R[0-9]|[T]N[0-9]|[C]R[0-9]|[S]M[0-9]|[K]T[0-9]|[T]W[0-9]|[H]A[0-9]|[U]B[0-9])'
+London_bookstore_name_regex = '(London -)'
 
 dataset_df = pd.read_csv('dataset.csv')
 dataset_df['Postcode'] = dataset_df[' Street_name'].str.extract(Postcode_regex)
 dataset_df = dataset_df.drop('Town', axis=1)
-filtered_column = dataset_df.Postcode.str.contains(London_postcode_regex)
-# print(filtered_column)
-dataset_df = dataset_df[dataset_df['Postcode'].str.contains(London_postcode_regex) == True]
+filtered_greater_london = dataset_df.Postcode.str.contains(Greater_london_postcode_regex)
+filtered_inner_london = dataset_df['Bookstore name'].str.contains(London_bookstore_name_regex)
+
+dataset_df['Greater_london'] = filtered_greater_london
+dataset_df['Inner_london'] = filtered_inner_london
+# dataset_df = dataset_df.drop(' Street_name', axis=1)
+
 
 print(dataset_df.head(10))
 
-# print(dataset_df.dtypes)
+# dataset_df.to_csv('filtered_dataset.csv', index=False)
