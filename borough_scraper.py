18a703b5a11c4e7e0a7f2b7024d4f1d8bfb52491
tobiolabode <tobi@tobiolabode.com>
2020-04-27 12:02:10 +0100

    working extracting borough names from postcodes.

--- borough_scraper.py
+++ borough_scraper.py
@@ -1,4 +1,24 @@
-import selenium
+from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 
 url = 'https://www.gov.uk/find-local-council'
+
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(15)
+print('Waiting ...')
+driver.get(url)
+
+inputElement = driver.find_element_by_id('postcode')
+inputElement = inputElement.send_keys('TW9 1TN')
+
+find_button = driver.find_element_by_xpath(
+    '/html/body/div[6]/div[1]/main/div/form/fieldset/div/button')
+find_button.click()
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+result_layer = soup.find('div', class_='unitary-result group')
+name_borough = result_layer.p.text
+
+print(name_borough)
