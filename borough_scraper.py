1f385d392e07d1628148b48bb03879df1563114d
tobiolabode <tobi@tobiolabode.com>
2020-04-27 14:31:44 +0100

    Finished extract borough names for dataset.

--- borough_scraper.py
+++ borough_scraper.py
@@ -1,24 +1,55 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+
+df = pd.read_csv('filtered_dataset.csv')
+list_postcodes = list(df['Postcode'])
+
+# print(list_postcodes)
+
+names_boroughs = []
 
 url = 'https://www.gov.uk/find-local-council'
 
-
 driver = webdriver.Firefox()
-driver.implicitly_wait(15)
-print('Waiting ...')
-driver.get(url)
 
-inputElement = driver.find_element_by_id('postcode')
-inputElement = inputElement.send_keys('TW9 1TN')
+i = 0
 
-find_button = driver.find_element_by_xpath(
-    '/html/body/div[6]/div[1]/main/div/form/fieldset/div/button')
-find_button.click()
+for postcode in list_postcodes:
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-result_layer = soup.find('div', class_='unitary-result group')
-name_borough = result_layer.p.text
+    print('Bookstore: {}'.format(df['Bookstore name'].iloc[i]))
+    i += 1
 
-print(name_borough)
+    driver.implicitly_wait(20)
+    print('Waiting ...')
+    driver.get(url)
+
+    inputElement = driver.find_element_by_id('postcode')
+    inputElement = inputElement.send_keys(postcode)
+
+    find_button = driver.find_element_by_xpath(
+        '/html/body/div[6]/div[1]/main/div/form/fieldset/div/button')
+    find_button.click()
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    result_layer = soup.find('div', class_='unitary-result group')
+    name_borough = result_layer.p.text
+
+    print(name_borough)
+    names_boroughs.append(name_borough)
+
+    print('Waiting ...')
+    time.sleep(10)
+
+    # driver.execute_script("window.history.go(-1)")
+
+
+driver.quit()
+
+df['Borough'] = names_boroughs
+
+print(df.head())
+
+df.to_csv('filtered_dataset_boroughs.csv', index=False)
